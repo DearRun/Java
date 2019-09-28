332d7191fdf8434568a821e2ab24da4b67fd95b5
Tiamoer <tiamoer@outlook.com>
2019-09-28 08:54:07 +0800

    Socket网络编程

--- 第一阶段/Client.java
+++ 第一阶段/Client.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("客户端初始化......");
+        System.out.println("客户端初始化.......");
         Socket client = new Socket("127.0.0.1",5000);
         System.out.println("连接服务器成功！");
 
